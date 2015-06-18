d9a271205ea1568f674cbac6c64600f14c5148dd
amber <amber@irs3-desktop>
2015-06-18 13:06:35 -0400

    function for mag field lines

---
+++ findmagline.m
@@ -0,0 +1,23 @@
+function [lat, lon, alt] = findmagline(GD)
+%% Desciption
+% this function calculates the magnetic lines for the input
+% geodata
+
+%% Inputs
+t = GD.times(:,1);
+t = squeeze(t);
+senloc = GD.sensorloc;
+
+% convert times
+newt = zeros(1,length(t));
+for i = 1:length(t)
+    newt(i) = datenum([1970 1 1 0 0 t(i)]);
+end
+
+%% Do work
+for i = 1:length(t)
+    [lat(:,i), lon(:,i), alt(:,i)] = igrfline(newt(i), senloc(1), senloc(2), ...
+        senloc(3),[], 90e3, 100);
+end
+
+end
---
+++ testaddmagline.m
@@ -0,0 +1,13 @@
+clear;
+clc;
+%% Test maglines
+
+%% Read Data
+risrName = 'ran120219.004.hdf5';
+risrGD = GeoData(@readMadhdf5,risrName,{'nel'});
+
+%% Do work
+[lat, lon, alt] = findmagline(risrGD);
+risrGD.data.maglines.lat = lat;
+risrGD.data.maglines.lon = lon;
+risrGD.data.maglines.alt = alt;
